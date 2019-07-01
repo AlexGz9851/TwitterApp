d2492ab22313ea2e3fe75667c8a29d9904690aba
alexgz9851 <zzz12300212@gmail.com>
2019-07-01 16:17:26 -0700

    Log in enabled

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,11 @@
+package com.codepath.apps.restclienttemplate.models;
+
+public class Tweet {
+    public String body;
+    public long uid; // database tweet id
+    public String createdDate;
+
+    // deserialize JSON
+
+    public
+}
